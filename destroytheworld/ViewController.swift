3240208bc655ada3710de798635e99df999e6c46
lucasm <lucasmagalhaesas@gmail.com>
2017-06-23 11:07:23 -0300

    added one line and excluded other
    Yes, I did it!

--- destroytheworld/ViewController.swift
+++ destroytheworld/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+print("it's just a test")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-        print("Hello world")"
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
